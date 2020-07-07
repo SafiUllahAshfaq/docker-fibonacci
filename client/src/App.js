e4919b3868dcc683dd63db23caf3e4ae0d3bafd4
SafiUllahAshfaq <safiu799@gmail.com>
2020-07-08 01:08:19 +0500

    react router in place

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
 import logo from './logo.svg';
 import './App.css';
+import OtherPage from './otherPage';
+import Fib from './Fib';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<div className='App'>
+				<header className='App-header'>
+					<img src={logo} className='App-logo' alt='logo' />
+					<p>
+						Edit <code>src/App.js</code> and save to reload.
+					</p>
+					<a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
+						Learn React
+					</a>
+					<Link to='/'>Home</Link>
+					<Link to='/otherpage'>Other Page</Link>
+				</header>
+
+				<div>
+					<Route exact path='/' component={Fib} />
+					<Route exact path='/otherpage' component={OtherPage} />
+				</div>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
